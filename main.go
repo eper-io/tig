f91abe6bb93269c15da66035a642f1e758281be3
Miklos Szegedi <hq@opensource.eper.io>
2024-04-19 18:34:24 -0700

    work

--- main.go
+++ main.go
@@ -64,14 +64,14 @@ func Setup() {
 	// Crashes or hangs should be fixed instead.
 	list, _ := os.ReadDir(root)
 	for _, v := range list {
-		if HashedFileValid(v.Name()) {
+		if IsValidTigHash(v.Name()) {
 			filePath := path.Join(root, v.Name())
 			ScheduleCleanup(filePath)
 		}
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if strings.Contains(r.URL.Path, "..") {
+		if strings.Contains(r.URL.Path, "..") || strings.HasPrefix(r.URL.Path, "./") {
 			w.WriteHeader(http.StatusExpectationFailed)
 			return
 		}
@@ -80,52 +80,24 @@ func Setup() {
 			if QuantumGradeAuthenticationFailed(w, r) {
 				return
 			}
-			buf := NoIssueApi(io.ReadAll(io.LimitReader(r.Body, MaxFileSize)))
-			shortName := fmt.Sprintf("%x.tig", sha256.Sum256(buf))
-			absolutePath := path.Join(root, shortName)
-			NoIssue(os.WriteFile(absolutePath, buf, 0600))
-			format := r.URL.Query().Get("format")
-			if format != "" {
-				relativePath := path.Join("/", shortName)
-				_, _ = io.WriteString(w, fmt.Sprintf(strings.Replace(format, "*", "%s", 1), relativePath))
-			}
-			ScheduleCleanup(absolutePath)
+			WriteStore(w, r)
 			return
 		}
 		if r.Method == "DELETE" {
-			if !HashedFileValid(r.URL.Path) {
+			if !IsValidTigHash(r.URL.Path) {
 				w.WriteHeader(http.StatusExpectationFailed)
 				return
 			}
 			if QuantumGradeAuthenticationFailed(w, r) {
 				return
 			}
-			if len(r.URL.Path) > 1 {
-				filePath := path.Join(root, r.URL.Path)
-				_, err := os.Stat(filePath)
-				if err != nil {
-					noAuthDelay.Lock()
-					time.Sleep(1 * time.Second)
-					noAuthDelay.Unlock()
-					w.WriteHeader(http.StatusNotFound)
-					return
-				}
-				// Normally you want to have a reasonable default period to verify files in your systems.
-				// Privacy may be a special case, when this is needed.
-				// Still, we do a day delay to prevent accidental tampering with live services.
-				// The deletion marker is still a hashed file that prevents it to show up but with apikey.
-				// An extra benefit of the logic is that the deletion marker is an addressed object.
-				// Deleted files can be returned without quantum grade waits for attackers.
-				// Coin usage checks can be very quick as a result.
-				deletion := []byte(fmt.Sprintf("%s0", r.URL.Path))
-				deletionPath := path.Join(root, fmt.Sprintf("%x.tig", sha256.Sum256(deletion)))
-				_ = os.WriteFile(deletionPath, deletion, 0600)
-				ScheduleCleanup(deletionPath)
+			if DeleteStore(w, r) {
+				return
 			}
 			return
 		}
 		if r.Method == "HEAD" {
-			if !HashedFileValid(r.URL.Path) {
+			if !IsValidTigHash(r.URL.Path) {
 				w.WriteHeader(http.StatusExpectationFailed)
 				return
 			}
@@ -141,79 +113,139 @@ func Setup() {
 				if QuantumGradeAuthenticationFailed(w, r) {
 					return
 				}
-				// TODO Disallow this on temporary tig stores w/o apikey
-				// TODO Audit this on long-term stores
-				f, _ := os.ReadDir(root)
-				// Newest file first
-				sort.SliceStable(f, func(i, j int) bool {
-					x, _ := f[i].Info()
-					y, _ := f[j].Info()
-					if x != nil && y != nil && x.ModTime().Before(y.ModTime()) {
-						return false
-					}
-					return true
-				})
-				format := r.URL.Query().Get("format")
-				for _, v := range f {
-					if strings.HasSuffix(v.Name(), ".tig") {
-						relativePath := path.Join("/", v.Name())
-						if format != "" {
-							relativePath = fmt.Sprintf(strings.Replace(format, "*", "%s", 1), relativePath)
-						}
-						NoIssueWrite(io.WriteString(w, relativePath+"\n"))
-					}
-				}
+				ListStore(w, r)
+				return
 			} else {
-				if !HashedFileValid(r.URL.Path) {
+				if !IsValidTigHash(r.URL.Path) {
 					w.WriteHeader(http.StatusExpectationFailed)
 					return
 				}
-				// Hashes are strong enough not to require an apikey
-				deletion := []byte(fmt.Sprintf("%s0", r.URL.Path))
-				deletionPath := path.Join(root, fmt.Sprintf("%x.tig", sha256.Sum256(deletion)))
-				_, err := os.Stat(deletionPath)
-				if err == nil {
-					w.WriteHeader(http.StatusNotFound)
+				if ReadStore(w, r) {
 					return
-				}
-				filePath := path.Join(root, r.URL.Path)
-				data, err := os.ReadFile(filePath)
-				if err != nil {
-					noAuthDelay.Lock()
-					time.Sleep(1 * time.Second)
-					noAuthDelay.Unlock()
-					w.WriteHeader(http.StatusExpectationFailed)
-					return
-				}
-				mimeType := r.URL.Query().Get("Content-Type")
-				if mimeType != "" {
-					w.Header().Set("Content-Type", mimeType)
-				} else {
-					w.Header().Set("Content-Type", "application/octet-stream")
-				}
-				NoIssueWrite(w.Write(data))
-				chTimes := r.URL.Query().Get("chtimes")
-				if chTimes != "0" {
-					go func(buf *[]byte) {
-						// allow reshuffling storage, and ensure security
-						fileName := path.Join(root, fmt.Sprintf("%x.tig", sha256.Sum256(*buf)))
-						//NoIssue(os.WriteFile(fileName, *buf, 0600))
-						// This prevents early cleanups of frequently used blobs
-						// It is equivalent to the accessed bit of x86 class processors
-						// Update modification time, allow first in first out cleanups,
-						// Make sure we have a simple & universal logic.
-						current := time.Now()
-						_ = os.Chtimes(fileName, current, current)
-						ScheduleCleanup(fileName)
-					}(&data)
 				}
 			}
-			return
 		}
 	})
 }
 
-func HashedFileValid(path string) bool {
+func ReadStore(w http.ResponseWriter, r *http.Request) bool {
+	// Hashes are strong enough not to require an apikey
+	deletion := []byte(fmt.Sprintf("%s0", r.URL.Path))
+	deletionPath := path.Join(root, fmt.Sprintf("%x.tig", sha256.Sum256(deletion)))
+	_, err := os.Stat(deletionPath)
+	if err == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return true
+	}
+	filePath := path.Join(root, r.URL.Path)
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		noAuthDelay.Lock()
+		time.Sleep(1 * time.Second)
+		noAuthDelay.Unlock()
+		w.WriteHeader(http.StatusExpectationFailed)
+		return true
+	}
+	mimeType := r.URL.Query().Get("Content-Type")
+	if mimeType != "" {
+		w.Header().Set("Content-Type", mimeType)
+	} else {
+		w.Header().Set("Content-Type", "application/octet-stream")
+	}
+	NoIssueWrite(w.Write(data))
+	chTimes := r.URL.Query().Get("chtimes")
+	if chTimes != "0" {
+		go func(buf *[]byte) {
+			// allow reshuffling storage, and ensure security
+			fileName := path.Join(root, fmt.Sprintf("%x.tig", sha256.Sum256(*buf)))
+			//NoIssue(os.WriteFile(fileName, *buf, 0600))
+			// This prevents early cleanups of frequently used blobs
+			// It is equivalent to the accessed bit of x86 class processors
+			// Update modification time, allow first in first out cleanups,
+			// Make sure we have a simple & universal logic.
+			current := time.Now()
+			_ = os.Chtimes(fileName, current, current)
+			ScheduleCleanup(fileName)
+		}(&data)
+	}
+	return false
+}
+
+func ListStore(w http.ResponseWriter, r *http.Request) {
+	referenceApiKey := os.Getenv("APIKEY")
+	if referenceApiKey == "" {
+		apiKeyContent, _ := os.ReadFile(path.Join(root, "apikey"))
+		if apiKeyContent != nil && len(apiKeyContent) > 0 {
+			referenceApiKey = strings.Trim(string(apiKeyContent), "\r\n")
+		}
+	}
+	if referenceApiKey == "" {
+		// Disallow this on temporary tig stores w/o apikey
+		return
+	}
+	// TODO Audit this on long-term stores
+	f, _ := os.ReadDir(root)
+	// Newest file first
+	sort.SliceStable(f, func(i, j int) bool {
+		x, _ := f[i].Info()
+		y, _ := f[j].Info()
+		if x != nil && y != nil && x.ModTime().Before(y.ModTime()) {
+			return false
+		}
+		return true
+	})
+	format := r.URL.Query().Get("format")
+	for _, v := range f {
+		if strings.HasSuffix(v.Name(), ".tig") {
+			relativePath := path.Join("/", v.Name())
+			if format != "" {
+				relativePath = fmt.Sprintf(strings.Replace(format, "*", "%s", 1), relativePath)
+			}
+			NoIssueWrite(io.WriteString(w, relativePath+"\n"))
+		}
+	}
+}
+
+func DeleteStore(w http.ResponseWriter, r *http.Request) bool {
+	if len(r.URL.Path) > 1 {
+		filePath := path.Join(root, r.URL.Path)
+		_, err := os.Stat(filePath)
+		if err != nil {
+			noAuthDelay.Lock()
+			time.Sleep(1 * time.Second)
+			noAuthDelay.Unlock()
+			w.WriteHeader(http.StatusNotFound)
+			return true
+		}
+		// Normally you want to have a reasonable default period to verify files in your systems.
+		// Privacy may be a special case, when this is needed.
+		// Still, we do a day delay to prevent accidental tampering with live services.
+		// The deletion marker is still a hashed file that prevents it to show up but with apikey.
+		// An extra benefit of the logic is that the deletion marker is an addressed object.
+		// Deleted files can be returned without quantum grade waits for attackers.
+		// Coin usage checks can be very quick as a result.
+		deletion := []byte(fmt.Sprintf("%s0", r.URL.Path))
+		deletionPath := path.Join(root, fmt.Sprintf("%x.tig", sha256.Sum256(deletion)))
+		_ = os.WriteFile(deletionPath, deletion, 0600)
+		ScheduleCleanup(deletionPath)
+	}
+	return false
+}
+
+func WriteStore(w http.ResponseWriter, r *http.Request) {
+	buf := NoIssueApi(io.ReadAll(io.LimitReader(r.Body, MaxFileSize)))
+	shortName := fmt.Sprintf("%x.tig", sha256.Sum256(buf))
+	absolutePath := path.Join(root, shortName)
+	NoIssue(os.WriteFile(absolutePath, buf, 0600))
+	format := r.URL.Query().Get("format")
+	if format != "" {
+		relativePath := path.Join("/", shortName)
+		_, _ = io.WriteString(w, fmt.Sprintf(strings.Replace(format, "*", "%s", 1), relativePath))
+	}
+	ScheduleCleanup(absolutePath)
+}
+
+func IsValidTigHash(path string) bool {
 	// We do not want to return anything else but hashed files
 	return strings.HasSuffix(path, ".tig") && len(path) > len(sha256.Sum256([]byte("")))
 }
